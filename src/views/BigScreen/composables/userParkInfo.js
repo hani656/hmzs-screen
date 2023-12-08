a89656400e9305e83d7244d442853ba89e965489
HaNi <2096420073@qq.com>
2023-12-08 11:01:08 +0800

    大屏项目

---
+++ src/views/BigScreen/composables/userParkInfo.js
@@ -0,0 +1,24 @@
+import { ref } from "vue"
+import {getParkInfoAPI} from '@/api/index'
+
+function useParkInfo(){
+    // 放置所有的业务逻辑
+    // 获取园区概况数据
+    const parkInfo = ref({})
+
+    async function getUserInfo (){
+    // 1. 调用接口
+    const res = await getParkInfoAPI()
+    // 2. 覆盖响应式数据
+    parkInfo.value = res.data
+    }
+    // 组件重要需要用到这些数据和方法
+    // 再组件中通过调用这个函数 得到这个对象
+    // 然后通过解构赋值的方法再把数据和方法引入到组件中使用
+    return {
+        parkInfo,
+        getUserInfo
+    }
+}
+
+export { useParkInfo }
